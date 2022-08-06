ebe16afc24d4023444b1001d047bc3cf00f2ae9a
LeeChangHoon <changhoon1030@gmail.com>
2022-08-07 01:54:14 +0900

    Feat : pagination

--- src/routes/Festival.js
+++ src/routes/Festival.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Card from "../components/Card"
+import Pagination from '../components/Pagination';
 
 const Festival = () => {
+    const [page, setPage] = useState(1);
+    const offset = (page - 1) * 10;
     const state = useSelector((state) => state.festivalReducer)
     const { festivalList, wishFestivalList } = state
     return (
         <div>
-            {festivalList.map((data) => <Card data={data} key={data.UC_SEQ}/> )}
+            {festivalList.slice(offset, offset + 10).map((data) => <Card data={data} key={data.UC_SEQ}/> )}
+            <footer>
+                <Pagination
+                    total={festivalList.length}
+                    limit={10}
+                    page={page}
+                    setPage={setPage}/>
+            </footer>
         </div>
     )
 }
--- src/routes/Restaurant.js
+++ src/routes/Restaurant.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import Card from "../components/Card"
+import Card from "../components/Card";
+import Pagination from '../components/Pagination';
+
 const Restaurant = () => {
+    const [page, setPage] = useState(1);
+    const offset = (page - 1) * 10;
+
     const state = useSelector((state) => state.restaurantReducer)
     const { restaurantList, wishRestaurantList } = state
     return (
         <div>
-            {restaurantList.map((data) => <Card data={data} key={data.UC_SEQ}/>)}
+            {restaurantList.slice(offset, offset + 10).map((data) => <Card data={data} key={data.UC_SEQ}/>)}
+            <footer>
+                <Pagination
+                    total={restaurantList.length}
+                    limit={10}
+                    page={page}
+                    setPage={setPage}/>
+            </footer>
         </div>
     )
 }
