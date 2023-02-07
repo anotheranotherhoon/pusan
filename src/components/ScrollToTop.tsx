43e34e9ce7389ee014607eefb1c7be6870591500
LeeChangHoon <changhoon1030@gmail.com>
2023-02-08 04:46:02 +0900

    ⚒ refactor : ScrollToTop 주석 제거

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -14,13 +14,13 @@ const ScrollToTop = () => {
             setBtnStatus(false)
         }
     }
-    const handleTop = () => {  // 클릭하면 스크롤이 위로 올라가는 함수
+    const handleTop = () => {  
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         });
-        setScrollY(0);  // ScrollY 의 값을 초기화
-        setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
+        setScrollY(0);  
+        setBtnStatus(false); 
     }
     useEffect(() => {
         const watch = () => {
